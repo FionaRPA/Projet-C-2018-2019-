faf64078cd48f692b23f77d8f5a367b73b6df096
Fiona <ravendran.fiona@gmail.com>
2018-12-26 11:14:30 +0100

    Fin de l'initialisation/ Image

---
+++ draw.h
@@ -0,0 +1,116 @@
+#include "structure.h"
+
+
+MLV_Color clanToColor(char clan){
+	switch(clan){
+		case ROUGE :
+			return MLV_COLOR_RED;
+		case BLEU :
+			return MLV_COLOR_BLUE;
+		default:
+			printf("error clan : %c\n", clan);
+			return MLV_COLOR_BLUE;
+	}
+}
+
+
+void drawIn(char* file, int largeur, int hauteur, int x, int y, char clan, char genre){
+	MLV_Image *image;
+	image = MLV_load_image(file);
+	MLV_resize_image_with_proportions( image, largeur/2, hauteur/2);
+	switch(genre){
+		case CHATEAU:
+			MLV_draw_image( image, (x*largeur)+10, (y*hauteur)+10);
+			MLV_draw_rectangle((x*largeur)+10, (y*hauteur)+10, largeur, hauteur, clanToColor(clan));
+			break;
+		case GUERRIER:
+			MLV_draw_image( image, (x*largeur)+10, (y*hauteur)+(hauteur/2)+10);
+			break;
+		case MANANT:
+			MLV_draw_image( image, (x*largeur)+(largeur/2)+10, (y*hauteur)+(hauteur/2)+10);	
+			break;
+		case BARON:
+			MLV_draw_image( image, (x*largeur)+(largeur/2)+10, (y*hauteur)+10);
+			break;
+		default:
+			printf("ERROR\n");
+			break;
+	}
+}
+
+
+void drawArray(Monde world){
+	int x, y;
+	int largeur = 60, hauteur = 60;
+	
+	for (x = 0 ; x < NBCOL ; x++) {
+		for (y = 0 ; y < NBLIG ; y++) {
+			Agent* chateau = world.plateau[x][y].chateau;
+			Agent* habitant = world.plateau[x][y].habitant;
+			//haut gauche
+			MLV_draw_rectangle((x*largeur)+10, (y*hauteur)+10, largeur/2, hauteur/2, MLV_rgba(120,120,120,255));
+			//haut droit
+			MLV_draw_rectangle((x*largeur)+(largeur/2)+10, (y*hauteur)+10, largeur/2, hauteur/2, MLV_rgba(120,120,120,255));
+			//bas gauche
+			MLV_draw_rectangle((x*largeur)+10, (y*hauteur)+(hauteur/2)+10, largeur/2, hauteur/2, MLV_rgba(120,120,120,255));
+			//bas droit
+			MLV_draw_rectangle((x*largeur)+(largeur/2)+10, (y*hauteur)+(hauteur/2)+10, largeur/2, hauteur/2, MLV_rgba(120,120,120,255));
+			//gros rectangle
+			MLV_draw_rectangle((x*largeur)+10, (y*hauteur)+10, largeur, hauteur, MLV_COLOR_WHITE);
+
+			if (chateau != NULL){
+				//CHATEAU
+				if (chateau->clan == ROUGE)
+					drawIn( "image/castleR.png", largeur, hauteur, chateau->posx, chateau->posy, chateau->clan, chateau->genre);
+				else
+					drawIn( "image/castleB.png", largeur, hauteur, chateau->posx, chateau->posy, chateau->clan, chateau->genre);
+			}
+			if (habitant != NULL){
+				switch(habitant->genre){
+					case GUERRIER:
+						if (habitant->clan == ROUGE)
+							drawIn("image/guerrierR.png", largeur, hauteur, habitant->posx, habitant->posy, habitant->clan, habitant->genre);
+						else
+							drawIn("image/guerrierB.png", largeur, hauteur, habitant->posx, habitant->posy, habitant->clan, habitant->genre);
+						break;
+					case MANANT:
+						if (habitant->clan == ROUGE)
+							drawIn("image/manantR.png", largeur, hauteur, habitant->posx, habitant->posy, habitant->clan, habitant->genre);
+						else
+							drawIn("image/manantB.png", largeur, hauteur, habitant->posx, habitant->posy, habitant->clan, habitant->genre);
+						break;
+					case BARON:
+						if (habitant->clan == ROUGE)
+							drawIn("image/baronR.png", largeur, hauteur, habitant->posx, habitant->posy, habitant->clan, habitant->genre);
+						else
+							drawIn("image/baronB.png", largeur, hauteur, habitant->posx, habitant->posy, habitant->clan, habitant->genre);
+						break;
+					default:
+						printf("ERROR\n");
+						break;
+				}
+			}
+		}
+	}
+}
+
+
+void drawBoxes(){
+	int x, y;
+	MLV_draw_text_box(L_FENETRE-200, H_FENETRE-700, 100, 30, "Attendre", 9, MLV_COLOR_VIOLET, MLV_COLOR_WHITE, MLV_COLOR_VIOLET, MLV_TEXT_CENTER, MLV_HORIZONTAL_CENTER, MLV_VERTICAL_CENTER);
+	MLV_draw_text_box(L_FENETRE-225, H_FENETRE-650, 150, 30, "Produire Baron", 9, MLV_COLOR_VIOLET, MLV_COLOR_WHITE, MLV_COLOR_VIOLET, MLV_TEXT_CENTER, MLV_HORIZONTAL_CENTER, MLV_VERTICAL_CENTER);
+	MLV_draw_text_box(L_FENETRE-225, H_FENETRE-600, 150, 30, "Produire Guerrier", 9, MLV_COLOR_VIOLET, MLV_COLOR_WHITE, MLV_COLOR_VIOLET, MLV_TEXT_CENTER, MLV_HORIZONTAL_CENTER, MLV_VERTICAL_CENTER);
+	MLV_draw_text_box(L_FENETRE-225, H_FENETRE-550, 150, 30, "Produire Manant" , 9, MLV_COLOR_VIOLET, MLV_COLOR_WHITE, MLV_COLOR_VIOLET, MLV_TEXT_CENTER, MLV_HORIZONTAL_CENTER, MLV_VERTICAL_CENTER);
+	MLV_draw_text_box(L_FENETRE-130, H_FENETRE-50, 100, 30, "Quitter" , 9, MLV_COLOR_WHITE, MLV_COLOR_BLACK, MLV_COLOR_WHITE, MLV_TEXT_CENTER, MLV_HORIZONTAL_CENTER, MLV_VERTICAL_CENTER);
+	MLV_actualise_window();
+	MLV_wait_mouse(&x, &y);
+
+	if((x < L_FENETRE-100 && x > L_FENETRE-200) && (y < H_FENETRE-670 && y > H_FENETRE-700))
+		printf("la\n");
+	else if((x < L_FENETRE-75 && x > L_FENETRE-225) && (y < H_FENETRE-620 && y > H_FENETRE-650))
+		printf("plo\n");
+	else if((x < L_FENETRE-75 && x > L_FENETRE-225) && (y < H_FENETRE-570 && y > H_FENETRE-600))
+		printf("moi\n");
+	else if((x < L_FENETRE-75 && x > L_FENETRE-225) && (y < H_FENETRE-520 && y > H_FENETRE-550))
+		printf("loi\n");
+}
--- projet.c
+++ projet.c
@@ -1,131 +1,52 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <MLV/MLV_all.h>
+#include "draw.h"
+#include "menu.h"
 
 
-/* dimension du monde en nombre de cases*/
-#define NBCOL 18
-#define NBLIG 12
+//																			INITIALISATION
 
-/* l’origine est en haut a gauche*/
-/* les deux clans */
-#define ROUGE 'R'
-#define BLEU  'B'
-#define LIBRE '-'
+Agent* createCastle(char couleur, int x, int y){
+	Agent* chateau = (Agent *) malloc(sizeof(Agent));
+	
+	if(chateau != NULL){
+		chateau->clan = couleur;
+		chateau->destx = chateau->desty = -1;
+		chateau->genre = CHATEAU;
 
-/* les genres d’agents */
-#define MANANT 'm'
-#define BARON  'b'
-#define GUERRIER  'g'
-#define CHATEAU 'c'
-
-/* les temps de production */
-#define TMANANT 2
-#define TGUERRIER 4
-#define TBARON 6
-
-/* les couts */
-#define CMANANT 1
-#define CGUERRIER 5
-#define CBARON 10
-#define CCHATEAU 30
-
-
-typedef struct agent{
-	char clan; /* ROUGE ou BLEU */
-	char genre; /* BARON, MANANT, GUERRIER, CHATEAU */
-	char produit; /* production actuelle d’un chateau*/
-	int temps; /* tours restant pour cette production */
-	int posx, posy; /* position actuelle */
-	int destx, desty; /* destination (negatif pour manant immobile) */
-	struct agent *asuiv, *aprec; /* liste des agents liees a un chateau */
-	struct agent *vsuiv, *vprec; /* liste des voisins */
-}Agent,*AListe;
-
-
-typedef struct{
-	Agent *chateau; /* s’il y a un chateau sur la case */
-	AListe habitant; /* les autres occupants */
-	char clan;/* couleur du clan ayant revendique*/
-}Case;
-
-
-typedef struct{
-	Case plateau[NBCOL][NBLIG];
-	AListe rouge, bleu;
-	int tour; /* Numero du tour */
-	int tresorRouge, tresorBleu;
-}Monde;
-
-
-
-void initCastle(char couleur, Agent *chateau){
-	chateau->clan = couleur;
-
-	chateau->genre = CHATEAU;
-	chateau->destx = chateau->desty = -1; //-1 pcq chateau immobile ?!
-
-	if (couleur == 'R'){
-		chateau->posx = 0;
-		chateau->posy = 0;
+		chateau->posx = x;
+		chateau->posy = y;
 	}
-
-	else{
-		chateau->posx = NBCOL - 1;
-		chateau->posy = NBLIG -1;
-	}
+	return chateau;
 }
 
 
-void initProduction(char couleur, Agent* agent, char genre){
-	agent->clan = couleur;
-
-	agent->genre = genre;
-
-	if (couleur == 'R'){
-		agent->posx = 0;
-		agent->posy = 0;
+Agent* createAgent(char couleur, char genre, int x, int y){
+	Agent* tmp;
+	
+	tmp = (Agent *) malloc(sizeof(Agent));
+	if (tmp != NULL){
+		tmp->clan = couleur;
+		tmp->genre = genre;
+		tmp->posx = x;
+		tmp->posy = y;
+		tmp->destx = tmp->desty = 0;
+		tmp->temps = 0;
+		tmp->asuiv = NULL;
+		tmp->aprec = NULL;
 	}
-
-	else{
-		agent->posx = NBCOL - 1;
-		agent->posy = NBLIG -1;
-	}
-
-	/*
-	//pour mannant immobile ?!
-	if (genre == 'm')
-		agent->destx = agent->desty = -1;
-	else{
-	*/
-	agent->destx = agent->desty = 0;
-	//}
-
-	agent->temps = 0;
+	return tmp;
 }
 
 
+void createMonde(Monde *world){
 
-void initMonde( Monde *world){
-/*
-	//initialisation des chateaux rouge et bleu;
-	Agent *chateauR = initcastle(ROUGE, chateauR), *chateauB;
-	world->plateau[0][0]->chateau = chateauR;
-	//world->plateau->initcastle(BLEU, &chateauB);
-
-	world->tour = 0;
-
-	world->tresorRouge = world->tresorBleu = 50;
-*/
 	int x, y;
 	Case cases;
-	cases.chateau = NULL;
-	cases.habitant = NULL;
+	cases.chateau = cases.habitant = NULL;
 	cases.clan = LIBRE;
 
 	for (x = 0 ; x < NBCOL ; x++) 
-		for (y = 0 ; y < NBLIG ; y++) 
-			world->plateau[x][y] = cases;
+		for (y = 0 ; y < NBLIG ; y++)
+ 			world->plateau[x][y]= cases;
 
 	world->tour = 0;
 	world->tresorBleu = world->tresorRouge = 50;
@@ -133,80 +54,51 @@ void initMonde( Monde *world){
 	world->bleu = NULL;
 }
 
-/*
-void afficheListe(Monde world){
-	int x, y;
-	for (x = 0 ; x < NBCOL ; x++){
-		for (y = 0 ; y < NBLIG ; y++){
-			if (world.plateau[x][y].chateau != NULL)
-				printf(" %c\n", world.plateau[x][y].chateau->clan);
-		}
-	}
-}
-*/
 
-
-void drawArray(Monde world, MLV_Image *image){
-	int x, y;
-	int largeur = 30, hauteur = 30;
-	int width = 30, height = 30;
-	
-	MLV_resize_image( image, width/2, height/2);
-
-	for (x = 0 ; x < NBCOL ; x++) {
-		for (y = 0 ; y < NBLIG ; y++) {
-
-			if (world.plateau[x][y].chateau != NULL){
-				if (world.plateau[x][y].chateau->clan == BLEU)
-					//MLV_draw_filled_circle((x*largeur)+(largeur/4)+10, (y*hauteur)+(hauteur/4)+10, 5, MLV_COLOR_BLUE);
-					MLV_draw_image(image, (x*largeur)+10, (y*hauteur)+10);
-
-				else if (world.plateau[x][y].chateau->clan == ROUGE)
-					MLV_draw_filled_circle((x*largeur)+(largeur/4)+10, (y*hauteur)+(hauteur/4)+10, 5, MLV_COLOR_RED);
-				
-			}
-			//haut gauche
-			MLV_draw_rectangle((x*largeur)+10, (y*hauteur)+10, largeur/2, hauteur/2, MLV_rgba(120,120,120,255));
-			//haut droit
-			MLV_draw_rectangle((x*largeur)+(largeur/2)+10, (y*hauteur)+10, largeur/2, hauteur/2, MLV_rgba(120,120,120,255));
-			//bas gauche
-			MLV_draw_rectangle((x*largeur)+10, (y*hauteur)+(hauteur/2)+10, largeur/2, hauteur/2, MLV_rgba(120,120,120,255));
-			//bas droit
-			MLV_draw_rectangle((x*largeur)+(largeur/2)+10, (y*hauteur)+(hauteur/2)+10, largeur/2, hauteur/2, MLV_rgba(120,120,120,255));
-			//gros rectangle
-			MLV_draw_rectangle((x*largeur)+10, (y*hauteur)+10, largeur, hauteur, /*(x == 10 && y == 10) ? MLV_COLOR_BLUE :*/ MLV_COLOR_WHITE);
-
-		}
-	}
-	MLV_actualise_window();
-}
-
+//																						MENU
 
 
 int main(int argc, char const *argv[]){
-	int h_fenetre = 400, l_fenetre = 800;
-	
-	MLV_create_window("Game of Stools", "Projet", l_fenetre, h_fenetre);
-	
-	MLV_Image *image;
-	image = MLV_load_image( "ChateauB.jpg" );
 
 	Monde m;
-	Agent chateauR, chateauB;
+	char genreR, genreB;
 
-	initMonde(&m);
-	initCastle(ROUGE, &chateauR);
-	initCastle(BLEU, &chateauB);
+	Agent* chateauR;
+	Agent* chateauB;
 
-	m.plateau[0][0].chateau = &chateauR;
-	m.plateau[NBCOL-1][NBLIG-1].chateau = &chateauB;
-	//afficheListe(m);
+	createMonde(&m);
+
+	chateauR = createCastle(ROUGE, 0, 0);
+	chateauB = createCastle(BLEU, NBCOL-1, NBLIG-1);
+
+	m.plateau[NBCOL-1][NBLIG-1].chateau = chateauB;
+	m.plateau[0][0].chateau = chateauR;
+
+	do{
+		printf("Quoi produire ? EQUIPE ROUGE\n");
+		scanf(" %c",&genreR);
+	}while(genreR != BARON && genreR != GUERRIER && genreR != CHATEAU && genreR != MANANT);
+
+	do{
+		printf("Quoi produire ? EQUIPE BLEU\n");
+		scanf(" %c",&genreB);
+	}while(genreB != BARON && genreB != GUERRIER && genreB != CHATEAU && genreB != MANANT);
+
+	Agent* agentR = createAgent(ROUGE, genreR, 0, 1);
+	Agent* agentB = createAgent(BLEU, genreB, NBCOL-1, NBLIG-2);
 	
-	drawArray(m, image);
+	m.plateau[agentB->posx][agentB->posy].habitant = agentB;
+	m.plateau[agentR->posx][agentR->posy].habitant = agentR;
 
-
-	MLV_wait_seconds(105000);
-	MLV_free_image(image);
+	MLV_create_window("Game Of Stools", "Projet", L_FENETRE, H_FENETRE);
+	menu();
+	MLV_clear_window(MLV_COLOR_BLACK);
+	MLV_actualise_window();
+	
+	drawArray(m);
+	drawBoxes();
+	MLV_actualise_window();
+	MLV_wait_seconds(20);
 	MLV_free_window();
 
 	return 0;
